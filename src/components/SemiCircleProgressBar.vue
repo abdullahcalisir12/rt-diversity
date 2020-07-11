5219aedfdce71b7d9f1c20d433762642142fc5fc
abdullahcalisir12 <abdullahcalisir12@gmail.com>
2020-07-11 13:24:16 +0300

    simple semi circle progress bar created

---
+++ src/components/SemiCircleProgressBar.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="single-chart">
+    <svg viewBox="0 0 36 36">
+      <g>
+        <path
+          d="M3,18 a15.9155,15.9155 0 0,1 31.831,0"
+          fill="none"
+          stroke-width="3"
+          stroke="#efefef"
+          stroke-linecap="round"
+        />
+        <path
+          class="circle"
+          d="M3,18 a15.9155,15.9155 0 0,1 31.831,0"
+          fill="none"
+          stroke-width="3"
+          stroke="orange"
+          stroke-linecap="round"
+          stroke-dasharray="25, 100"
+        />
+      </g>
+    </svg>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+@Component
+export default class SemiRadialChart extends Vue {}
+</script>
+
+<style>
+.single-chart {
+  width: 100%;
+  max-width: 400px;
+}
+
+.circle {
+  animation: progress 1s ease-out forwards;
+}
+
+@keyframes progress {
+  0% {
+    stroke-dasharray: 0 100;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,23 @@
 <template>
   <div class="home">
     <h1>{{ msg }}</h1>
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <div>
+      <SemiCircleProgressBar />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from "vue-property-decorator";
 import Api from "@/airtable-api";
-@Component
+
+import SemiCircleProgressBar from "@/components/SemiCircleProgressBar.vue";
+
+@Component({
+  components: {
+    SemiCircleProgressBar
+  }
+})
 export default class Home extends Vue {
   private msg = "Hello Remote Team";
   private apiKey = process.env.VUE_APP_AIRTABLE_API_KEY;
