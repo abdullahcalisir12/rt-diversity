4148b0fa28cf525bb777553a3b9f36fbd1533ce3
abdullahcalisir12 <abdullahcalisir12@gmail.com>
2020-07-12 14:31:37 +0300

    bar chart value, label and color props added

--- src/components/BarChart.vue
+++ src/components/BarChart.vue
@@ -1,28 +1,37 @@
 <template>
   <div class="single-chart">
-    <svg width="420" :height="chartHeight">
+    <svg width="400" :height="chartHeight">
       <rect
-        v-for="n in total"
-        :key="n"
+        v-for="(label, index) in labels"
+        :key="index"
         x="0"
-        :y="(n - 1) * barHeight + n * gap"
-        :width="(420 * 1) / n"
+        :y="index * barHeight + index * gap"
+        :width="total > 0 ? values[index] * 100 / total : 0"
         :height="barHeight"
+        :fill="colors[index]"
       ></rect>
     </svg>
   </div>
 </template>
 
 <script lang="ts">
-import { Vue, Component } from "vue-property-decorator";
+import { Vue, Component, Prop } from "vue-property-decorator";
 @Component
 export default class BarChart extends Vue {
+  @Prop({ required: true }) readonly labels!: Array<string>;
+  @Prop({ required: true }) readonly values!: Array<number>;
+  @Prop({ required: true }) readonly colors!: Array<string>;
+
+
   private chartHeight = 300;
   private gap = 10;
-  private total = 12;
 
   private barHeight: number =
-    (this.chartHeight - this.total * this.gap) / this.total;
+    (this.chartHeight - this.values.length * this.gap) / this.values.length;
+
+  get total(): number {
+    return this.values && this.values.reduce((acc, cur) => acc = acc + cur);
+  }
 }
 </script>
 
