348d7d75aceb155efde591adf0bcc89c21863333
abdullahcalisir12 <abdullahcalisir12@gmail.com>
2020-07-12 10:55:42 +0300

    selectbox component created

---
+++ src/components/SelectBox.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="select-box">
+    <select>
+      <option value="">Please Select One</option>
+      <option
+        v-for="option in options"
+        :key="option.value"
+        :value="option.value"
+        >{{ option.name }}</option
+      >
+    </select>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from "vue-property-decorator";
+@Component
+export default class SelectBox extends Vue {
+  @Prop({ required: true }) readonly options: Array<object>;
+}
+</script>
